9f46f9133856778a8aa13462950e9e23ad431a1f
Katherine-valcarce <katherine.v@live.cl>
2021-05-29 17:58:57 -0400

    TARJETA VALIDA OK

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import validator from './validator.js';
 
+document.getElementById("Validacion").addEventListener("click",myFunction);
+
+function myFunction(){
+    validator.isvalid();
+    validator.maskify();
+}
+
 console.log(validator);
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,33 @@
 const validator = {
-  // ...
+  isvalid:function(){
+let NumberCard1 = document.getElementById("NumberCard1").value;
+let Box = Array.from(NumberCard1);
+let NumberSum=0;
+
+ for(let i=0;i<NumberCard1.length;i++){
+  Box[i]=parseInt(Box[i]);
+ }
+ Box=Box.reverse();
+
+  for(let i=1;i<Box.length;i=i+2){
+   Box[i]=Box[i]*2;
+   if(Box[i]>9){ Box[i]= 1+Box[i]%10;
+  }
+ }
+
+ for(let i=0;i<Box.length;i++)
+
+var Sum = (new Function("return " +Box.join('+')))();
+var resultado = Sum%10;
+
+if (resultado==0){
+  alert("TARJETA VALIDA");
+} else { alert("TARJETA INVALIDA")
+}
+
+ },
+
 };
+
 
 export default validator;
