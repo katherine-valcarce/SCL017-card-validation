133357da928428abea3201fd003171ca896b598d
Katherine-valcarce <katherine.v@live.cl>
2021-06-01 17:07:35 -0400

    guardado2

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import validator from './validator.js';
 
+let creditCardNumber = document.getElementById("NumberCard1").value;
+
+document.getElementById("Validacion").addEventListener("click",myFunction2);
+
+function myFunction2(){
+    validator.maskify(creditCardNumber);
+}
+//llamamos al boton id=Validacion si esto es ejecutado al hacer "click" se ejecuta la funcion myFunction
 document.getElementById("Validacion").addEventListener("click",myFunction);
 
 function myFunction(){
-    validator.isvalid();
-    validator.maskify();
+    validator.isvalid(creditCardNumber);
 }
 
-console.log(validator);
--- src/validator.js
+++ src/validator.js
@@ -1,33 +1,44 @@
-const validator = {
-  isvalid:function(){
-let NumberCard1 = document.getElementById("NumberCard1").value;
-let Box = Array.from(NumberCard1);
-let NumberSum=0;
 
- for(let i=0;i<NumberCard1.length;i++){
-  Box[i]=parseInt(Box[i]);
+function isvalid(creditCardNumber){
+ // transformamos el NumberCard1 en un array para que cada digito quede separado
+ let Box = Array.from(creditCardNumber);
+ //se crea el bucle FOR para que recorra cada string ingresado y lo convierta en numero
+ for(let i=0;i<creditCardNumber.length;i++){
+      // transformamos cada string del bucle a numero
+     Box[i]=parseInt(Box[i]);
  }
- Box=Box.reverse();
-
-  for(let i=1;i<Box.length;i=i+2){
-   Box[i]=Box[i]*2;
-   if(Box[i]>9){ Box[i]= 1+Box[i]%10;
+ //reversamos esta lista de numeros
+ let BoxReverse=Box.reverse();
+ //con el bucle for tomamos todos los numeros pares
+  for(let i=1;i<BoxReverse.length;i=i+2){
+    //estos numeros pares los multiplicamoms por 2
+   BoxReverse[i]=BoxReverse[i]*2;
+   // si resultado de la multiplicacion es mayor a 9 debe sumar 1 más el residuo de ese resultado
+   if(BoxReverse[i]>9){ BoxReverse[i]= 1+BoxReverse[i]%10;
   }
  }
-
- for(let i=0;i<Box.length;i++)
-
-var Sum = (new Function("return " +Box.join('+')))();
-var resultado = Sum%10;
-
-if (resultado==0){
+ //Volvemos a generar un bucle FOR para tomar todos los numeros ingresados (ya modificados)
+ for(let i=0;i<BoxReverse.length;i++)
+ //creamos la variable Sum para sumar toda la cadena de numeros del array
+ var Sum = (new Function("return " +BoxReverse.join('+')))();
+ //creamos la variable resultado donde es igual al residuo de la suma total divido en 10
+ var resultado = Sum%10;
+ // creamos una condicion donde establece que si el resultado es 0 esto es True y nos crea un alert de tarjeta valida
+ if (resultado==0){
   alert("TARJETA VALIDA");
+  //si la condicion anterior es falta o distinta a cero sería False y nos crea un alert de tarjeta invalida
 } else { alert("TARJETA INVALIDA")
 }
 
- },
-
-};
-
-
-export default validator;
+ };
+ function maskify(creditCardNumber){
+   cardNumberMaskify = Array.from(creditCardNumber)
+   let replaced =  cardNumberMaskify[i].replace(/.(?=.{4,}$)/g, "#");
+   document.write(replaced)
+  /*return replaced;*/
+    }
+   
+    
+  
+const validator = {isvalid,maskify};
+ export default validator;
